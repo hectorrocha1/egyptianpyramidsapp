eacb724fcc2d44783447ca99a78d52725d8aceaf
Hector Rocha <hectorrocha41203@gmail.com>
2022-12-09 22:27:20 -0800

    created maven project

---
+++ egyptianpyramids/src/main/java/hectorrocha/App.java
@@ -0,0 +1,18 @@
+//Hector Rocha
+// CSIS 26
+//egyptianpyramidsapp
+
+
+package hectorrocha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
